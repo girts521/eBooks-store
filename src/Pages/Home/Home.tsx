5c3ea474fec424acaf426ebf885c8330588f17a1
girts521 <girts521@gmail.com>
2022-07-29 11:09:01 +0200

    improved course images, started code  cleanup

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,20 +1,12 @@
 import React from "react";
+import Header from "../../Components/Header/Header";
 import styles from "./styles.module.scss";
 
 const Home: React.FC = () => {
   return (
     <>
-      <div className={styles.header}>
-        <div className={styles.headerText}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique,
-            exercitationem cum! Ab, recusandae numquam, dignissimos in fuga quas
-            praesentium ipsam dolorum neque nam, modi sequi veritatis libero
-            corrupti eligendi quo.
-          </p>
-          <div className={styles.headerButton}>SHOP NOW!</div>
-        </div>
-      </div>
+    <Header />
+
       <div className={styles.container}>
         <div className={styles.aboutUs}>
           <h1>ABOUT US</h1>
@@ -31,57 +23,61 @@ const Home: React.FC = () => {
         </div>
 
         <div className={styles.courses}>
-          <div className={styles.courseCard}>
-            <div className={styles.courseImg}>
-              <img src="./peak-performance.jpg" alt="" />
+          <div className={styles.firstChild}>
+            <div className={styles.courseCard}>
+              <div className={styles.courseImg}>
+                <img src="./personal-development.jpg" alt="" />
+              </div>
+              <div className={styles.courseName}>
+                Learn how to keep improving yourself
+              </div>
+              <div className={styles.courseDescription}>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+                modi sit unde velit suscipit nostrum, repellat aut consectetur
+                odit possimus ad quia, quam rem eos commodi cumque iste atque
+                minus.
+              </div>
+              <div className={styles.courseBtn}>LEARN MORE</div>
             </div>
-            <div className={styles.courseName}>
-              Learn secrets of peak performance
-            </div>
-            <div className={styles.courseDescription}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
-              modi sit unde velit suscipit nostrum, repellat aut consectetur
-              odit possimus ad quia, quam rem eos commodi cumque iste atque
-              minus.
-            </div>
-            <div className={styles.courseBtn}>LEARN MORE</div>
           </div>
 
-          <div className={styles.courseCard}>
-            <div className={styles.courseImg}>
-              <img src="./personal-development.jpg" alt="" />
+          <div className={styles.secondChild}>
+            <div className={styles.courseCard}>
+              <div className={styles.courseImg}>
+                <img src="./peak-performance.jpg" alt="" />
+              </div>
+              <div className={styles.courseName}>
+                Learn secrets of peak performance
+              </div>
+              <div className={styles.courseDescription}>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+                modi sit unde velit suscipit nostrum, repellat aut consectetur
+                odit possimus ad quia, quam rem eos commodi cumque iste atque
+                minus.
+              </div>
+              <div className={styles.courseBtn}>LEARN MORE</div>
             </div>
-            <div className={styles.courseName}>
-              Learn how to keep improving yourself
-            </div>
-            <div className={styles.courseDescription}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
-              modi sit unde velit suscipit nostrum, repellat aut consectetur
-              odit possimus ad quia, quam rem eos commodi cumque iste atque
-              minus.
-            </div>
-            <div className={styles.courseBtn}>LEARN MORE</div>
-          </div>
 
-          <div className={styles.courseCard}>
-            <div className={styles.courseImg}>
-              <img src="./public-speaking.jpg" alt="" />
+            <div className={styles.courseCard}>
+              <div className={styles.courseImg}>
+                <img src="./public-speaking.jpg" alt="" />
+              </div>
+              <div className={styles.courseName}>
+                Learn how to master public speaking in 5 minutes
+              </div>
+              <div className={styles.courseDescription}>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+                modi sit unde velit suscipit nostrum, repellat aut consectetur
+                odit possimus ad quia, quam rem eos commodi cumque iste atque
+                minus.
+              </div>
+              <div className={styles.courseBtn}>LEARN MORE</div>
             </div>
-            <div className={styles.courseName}>
-              Learn how to master public speaking in 5 minutes
-            </div>
-            <div className={styles.courseDescription}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
-              modi sit unde velit suscipit nostrum, repellat aut consectetur
-              odit possimus ad quia, quam rem eos commodi cumque iste atque
-              minus.
-            </div>
-            <div className={styles.courseBtn}>LEARN MORE</div>
           </div>
         </div>
         <div className={styles.viewAll}>
-            <p>VIEW ALL COURSES</p>
-          </div>
+          <p>VIEW ALL COURSES</p>
+        </div>
 
         <div className={styles.reviews}>
           <p className={styles.reviewText}>
